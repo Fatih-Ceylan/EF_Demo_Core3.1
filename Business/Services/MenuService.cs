f3b8969ad03d9ed72af0ad449289ee660f8f6120
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-22 21:32:14 +0300

    Update MenuService.cs

--- Business/Services/MenuService.cs
+++ Business/Services/MenuService.cs
@@ -33,7 +33,6 @@ namespace Business.Services
                 Id = r.Id
             }).ToList();
         }
-
         /// null dönüyor
         //public List<MenuDto> DeleteMenu(int id)
         //{
@@ -43,7 +42,6 @@ namespace Business.Services
         //    menus.Remove(menu);
         //    return (menus);
         //}
-
         public MenuDto DeleteMenu(long id)
         {
             var result = new MenuDto();
@@ -58,7 +56,6 @@ namespace Business.Services
             result.IsDeleted = true;
             return result;
         }
-
         //public MenuDto DeleteMenu(long id)
         //{
         //    var delete = _menuRepository.DeleteMenu(id);
